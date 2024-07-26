af4c6d35df883517c4a98ee6b134497e8dd97ba6
Saloni-FED <salonipandey014@gmail.com>
2024-07-26 14:13:05 +0530

    changes in index page

--- pages/index.js
+++ pages/index.js
@@ -1,39 +1,42 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import Banner from '../components/HomeOne/Banner';
-import Funfact from '../components/Common/Funfact';
-import BuySell from '../components/HomeOne/BuySell';
-import AccountCreate from '../components/Common/AccountCreate';
-import Platform from '../components/HomeOne/Platform';
-import FeedBack from '../components/HomeOne/FeedBack';
-import Portfolio from '../components/Common/Portfolio';
-import OurFeature from '../components/Common/OurFeature';
-import AppDownload from '../components/Common/AppDownload';
-import RegisterAreaTwo from '../components/Common/RegisterAreaTwo';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Banner from "../components/HomeOne/Banner";
+import Funfact from "../components/Common/Funfact";
+import BuySell from "../components/HomeOne/BuySell";
+import AccountCreate from "../components/Common/AccountCreate";
+import Platform from "../components/HomeOne/Platform";
+import FeedBack from "../components/HomeOne/FeedBack";
+import Portfolio from "../components/Common/Portfolio";
+import OurFeature from "../components/Common/OurFeature";
+import AppDownload from "../components/Common/AppDownload";
+import RegisterAreaTwo from "../components/Common/RegisterAreaTwo";
 
 const Index = () => {
-  
   return (
     <>
       <Banner />
 
-      {/* <Funfact pt100='pt-100' /> */}
+      <div style={{ paddingTop: "10rem" }}>
+        <OurFeature title="Our Features" />
+      </div>
+      <div style={{paddingBottom:"10rem"}}>
+      <Funfact pt100="pt-100" />
+        
+      </div>
 
-      <BuySell pt70='pt-70' />
+      <BuySell pt70="pt-70" />
 
-      <AccountCreate title='Get Started in a Few Minutes' />
+      <AccountCreate title="Get Started in a Few Minutes" />
 
       <Platform />
 
       <FeedBack />
 
-      <Portfolio bgColor='bg-f9f9f9' />
-
-      <OurFeature title='Our Features' />
+      {/* <Portfolio bgColor="bg-f9f9f9" />
 
       <AppDownload />
-      
-      <RegisterAreaTwo title='Start Trading on Novis' />
+
+      <RegisterAreaTwo title="Start Trading on Novis" /> */}
     </>
   );
 };
