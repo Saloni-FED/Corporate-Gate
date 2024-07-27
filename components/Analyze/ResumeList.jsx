0a52c7f04e1bc331049a1dc94b07c25bdb88e669
Saloni-FED <salonipandey014@gmail.com>
2024-07-27 14:39:09 +0530

    done styling

--- components/Analyze/ResumeList.jsx
+++ components/Analyze/ResumeList.jsx
@@ -6,19 +6,21 @@ const ResumeList = () => {
     { name: 'Samaira Singh', date: '12-04-2024' },
     { name: 'Samaira Singh', date: '12-04-2024' },
     { name: 'Samaira Singh', date: '12-04-2024' },
+    { name: 'Samaira Singh', date: '12-04-2024' },
+    { name: 'Samaira Singh', date: '12-04-2024' },
     { name: 'Samaira Singh', date: '12-04-2024' }
   ];
 
   return (
-    <div className="custom-resume-list">
+    <div className="custom-resume-list" >
       <h2 style={{paddingLeft:"20px"}}>Recent Resume</h2>
       <ul>
         {resumes.map((resume, index) => (
-          <li key={index} style={{padding:"1rem 2rem"}}>
+          <li key={index} >
             <span>{index + 1}.</span>
             <span>{resume.name}</span>
             <span>{resume.date}</span>
-            <img src="/pdf.png" alt="" srcset="" style={{width:"2rem"}}/>
+            <img src="/pdf.png" alt="" srcset="" />
             <div className="custom-actions">
               <a href="#view">View</a>
               <a href="#edit">Edit</a>
